11b95fe6db24989ec190e61995a1edb6c742802c
Asijit Paul <asijit1610@gmail.com>
2021-01-19 15:09:08 +0530

    test function added

---
+++ php/__test__/get_lat_long.php
@@ -0,0 +1,27 @@
+<?php
+function getCord($address){
+$key = 'jawgmaps_api_key';
+
+$url = 'https://api.jawg.io/places/v1/search?text='.urlencode($address).'&access-token='.$key.'&size=1';
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$responseJson = curl_exec($ch);
+curl_close($ch);
+
+$response = json_decode($responseJson, true);
+
+$location = array(
+	'x' => $response['features']['0']['geometry']['coordinates']['1'],
+    'y' => $response['features']['0']['geometry']['coordinates']['0']
+);
+
+return $location;
+ }
+?>
